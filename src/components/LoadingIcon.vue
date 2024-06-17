d204ea6f3d40b404a20979bc06d69038d4120555
scarlett-troncoso <scar.labussieri@gmail.com>
2024-06-17 19:04:29 +0200

    create new component for icon loading

---
+++ src/components/LoadingIcon.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'LoadingIcon.vue'
+}
+</script>
+
+<template>
+    <div class="loader">
+        <i class="fa-solid fa-spinner fa-spin"></i>
+        <div>loading</div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.loader {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
