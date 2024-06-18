7b23944cef1285f2f0681b3fce83e192656469f6
scarlett-troncoso <scar.labussieri@gmail.com>
2024-06-18 03:34:29 +0200

    import store js, and create filter of search input

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,54 +1,82 @@
 <script>
+import { store } from '../store';
 export default {
     name: 'AppHeader.vue',
+    //emits: ['search'],
+
+    /*components: {
+        //Search
+    },*/
+
+    data() {
+        return{
+            store,
+            search_text: ''
+        }
+    },
+
+    methods: {
+        search(){ //(data)
+            store.loading = true;
+            //console.log('search_text', data);
+            //const [search_text] = data;
+            const url_search = store.base_api_url + store.photos_endpoint + `?search=${this.search_text}`
+            console.log(url_search); // console: http://127.0.0.1:8000/api/posts?search=sfrefe 
+            store.callApiPhotos(url_search)
+        },
+    }
 }
 </script>
 
+
 <template>
     <header class="side-header">
-        <nav class="navbar navbar-expand-lg navbar-header"> <!--bg-dark  bg-body-tertiary--->
+        <nav class="navbar navbar-expand-lg navbar-header">
             <div class="container-fluid m-2">
-                <div class="mx-3">
+                <div class="mx-3 navbar-brand">
                     <h2>FotoAlbum</h2>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <!--<li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav"> <!--navbarSupportedContent-->
+                    <ul class="navbar-nav"> <!--me-auto mb-2 mb-lg-0-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a> <!--link-dark link-opacity-25-hover link-underline-opacity-0 link-underline-opacity-25-hover"-->
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                        </li>-->
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#per-il-login" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Login
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Login or Register</a></li>
-                                <!--<li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>-->
-                            </ul>
-                            <!--</li>
-                            <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>-->
+                            <a class="nav-link" href="#">Other</a>
                         </li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Cerca per titolo" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Cerca</button>
-                    </form>
+                    
                 </div>
+                <form @submit.prevent="search" class="mb-3 d-flex cerca">
+                    <div class="input-group"> 
+                        <input type="search" class="form-control input-cerca" placeholder="Cerca per titolo" aria-label="Search"  v-model="search_text">
+                        <button class="btn btn-outline-light btn-cerca" type="submit" ><i class="fa fa-search fa-lg fa-fw"  ></i></button> 
+                    </div>
+                </form>
+
+                <!--<Search @search="search"></Search>-->
             </div>
-        </nav>
+        </nav>        
     </header>
 </template>
 
 <style scoped>
     .navbar-header{
         background-color: transparent;
+    }
+
+    /*.cerca{
+        width: 30%;
+    }*/
+    
+    .input-cerca {
+        background-color: transparent;
+    }
+
+    .btn-cerca{
+        
     }
 </style>
